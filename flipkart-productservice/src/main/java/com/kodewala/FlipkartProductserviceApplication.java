3284052cd73532180f89dd049b1a153ca2ad865f
janardancode <janardanbihta123@gmail.com>
2026-01-05 14:38:41 +0530

    this is flipkart project service

---
+++ flipkart-productservice/src/main/java/com/kodewala/FlipkartProductserviceApplication.java
@@ -0,0 +1,13 @@
+package com.kodewala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipkartProductserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipkartProductserviceApplication.class, args);
+	}
+
+}
---
+++ flipkart-productservice/src/test/java/com/kodewala/FlipkartProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodewala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipkartProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
